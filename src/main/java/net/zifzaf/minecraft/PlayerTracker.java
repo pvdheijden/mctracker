85ea217eb28e8bf4b6f619e7a3fedf04f2206a5b
pim <pim@zifzaf.com>
2020-03-25 09:40:29 +0100

    firestore support

--- src/main/java/net/zifzaf/minecraft/PlayerTracker.java
+++ src/main/java/net/zifzaf/minecraft/PlayerTracker.java
@@ -1,27 +1,23 @@
 package net.zifzaf.minecraft;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlayerTracker extends JavaPlugin {
-    final static String ONLINE_KEY = "online";
-    final static String PLAYER_KEY = "player:%s";
-    final static String LOCATION_TIMESTAMP = "timestamp";
-    final static String LOCATION_X_KEY = "x";
-    final static String LOCATION_Y_KEY = "y";
-    final static String LOCATION_Z_KEY = "z";
-
     public PlayerTracker() {
         super();
     }
+
+    Firestore database;
 
     @Override
     public void onEnable() {
@@ -29,13 +25,15 @@ public class PlayerTracker extends JavaPlugin {
 
         FileInputStream serviceAccount;
         try {
-            serviceAccount = new FileInputStream("minecraft-zifzaf-net-firebase-adminsdk-bgo5k-1584b78f44");
+            serviceAccount = new FileInputStream("firebase-adminsdk.json");
 
             FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .setDatabaseUrl("https://minecraft-zifzaf-net.firebaseio.com").build();
 
             FirebaseApp.initializeApp(options);
+            
+            database = FirestoreClient.getFirestore();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
